66e2fc3a226f889858909b42b1760f7bd95172d0
Deva Koushal <rebullrock@gmail.com>
2017-01-10 23:02:49 +0530

    Add files via upload

---
+++ SpeechRecognition/SpeechRecognition/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SpeechRecognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ENABLE = new System.Windows.Forms.Button();
+            this.DISABLE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(344, 195);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "-HELLO-";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // ENABLE
+            // 
+            this.ENABLE.Location = new System.Drawing.Point(12, 230);
+            this.ENABLE.Name = "ENABLE";
+            this.ENABLE.Size = new System.Drawing.Size(131, 36);
+            this.ENABLE.TabIndex = 1;
+            this.ENABLE.Text = "EnableVoiceControl";
+            this.ENABLE.UseVisualStyleBackColor = true;
+            this.ENABLE.Click += new System.EventHandler(this.ENABLE_Click);
+            // 
+            // DISABLE
+            // 
+            this.DISABLE.Enabled = false;
+            this.DISABLE.Location = new System.Drawing.Point(225, 230);
+            this.DISABLE.Name = "DISABLE";
+            this.DISABLE.Size = new System.Drawing.Size(131, 36);
+            this.DISABLE.TabIndex = 2;
+            this.DISABLE.Text = "DisableVoiceControl";
+            this.DISABLE.UseVisualStyleBackColor = true;
+            this.DISABLE.Click += new System.EventHandler(this.DISABLE_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 278);
+            this.Controls.Add(this.DISABLE);
+            this.Controls.Add(this.ENABLE);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "chitty";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button ENABLE;
+        private System.Windows.Forms.Button DISABLE;
+    }
+}
+
---
+++ SpeechRecognition/SpeechRecognition/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace SpeechRecognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ENABLE_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            DISABLE.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "hello mark", "what's my name", "i'am fine", "fuck yourself", "go fuck yourself", "who are you","what's your name", "what are you doing", "speak this", "you are idiot" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            //recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+        }
+
+        void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch(e.Result.Text)
+            {
+                case "hello mark":
+                    synthesizer.SpeakAsync("Hello sir. How are you?");
+                    break;
+                case "i'am fine":
+                    synthesizer.SpeakAsync("that's good");
+                    break;
+                case "what's my name":
+                    synthesizer.SpeakAsync("DEVA KOUSHAL");
+                    break;
+                case "what's your name":
+                    synthesizer.SpeakAsync(" MY NAME is mark");
+                    break;
+                case "speak this":
+                    synthesizer.SpeakAsync(richTextBox1.SelectedText);
+                    break;
+                case "who are you":
+                    synthesizer.SpeakAsync("Hello!, i am mark");
+                    break;
+                case "what are you doing":
+                    synthesizer.SpeakAsync("I am just compiling ,sir");
+                    break;
+                case "you are idiot":
+                    synthesizer.SpeakAsync("that was rude...");
+                    break;
+                case "fuck yourself":
+                    synthesizer.SpeakAsync("Sorry i can't");
+                    break;
+                case "go fuck yourself":
+                    synthesizer.SpeakAsync("I'am not a human to do that, i'am a program!");
+                    break;
+            }
+        }
+
+        private void DISABLE_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            DISABLE.Enabled = false;
+        }
+    }
+}
